ecc0c8ce31bddb4587d8a99c7eba782065d3c62f
Dan G. Switzer, II <dswitzer@pengoworks.com>
2016-11-21 18:05:31 -0500

    Added xauth to resolve SSH issues
    This fixes "X11 forwarding request failed on channel" issues with SSH
    commands

--- scripts/100-os-update.sh
+++ scripts/100-os-update.sh
@@ -7,4 +7,5 @@ yum $YUM_OPTIONS update
 yum $YUM_OPTIONS upgrade
 
 # install extra packages
-yum $YUM_OPTIONS install epel-release unzip
+# xauth - This fixes "X11 forwarding request failed on channel" issues with SSH commands: https://www.cyberciti.biz/faq/how-to-fix-x11-forwarding-request-failed-on-channel-0/
+yum $YUM_OPTIONS install epel-release unzip xauth
